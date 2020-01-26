860c58d8269a0a9a79f2b53f1c398f33defa1851
Molonlabee <janayahernandez@yahoo.com>
2020-01-25 20:07:24 -0600

    js-4

--- classes.js
+++ classes.js
@@ -30,6 +30,20 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  } 
+  
+  makeWidget() {
+  
+   return `${this.first_name} ${this.last_name} Widget`;
+ 
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -48,6 +62,20 @@
 */
 
 //Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age); 
+      this.reports = [];
+  }   
+
+    hire(employee) {
+     this.reports.push(employee);
+    }
+    fire(index) {
+      this.reports.splice(index, 1);
+    }
+}
+
 
 
 ////////// PROBLEM 3 //////////
@@ -72,7 +100,53 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age);
+    this.title = 'Not a manager';
+    this.bonus = 0; 
+  }
+  hire(employee) {
+    super.hire(employee);
+    if(this.reports.length === 0 ) {
+      return this.title = 'Not a manager';
+    } else if(this.reports.length >= 1 && this.reports.length <=3 ){
+      return this.title = 'Barely Manager';
 
+    }else if(this.reports.length >= 4 && this.reports.length <=10 ){
+      return this.title = 'Mostly Manager';
+    }else if(this.reports.length >= 11 && this.reports.length <=50 ){
+      return this.title = 'Manager';
+    }else if(this.reports.length >= 51 && this.reports.length <=100 ){
+      return this.title = 'Manager Plus';
+    }else if(this.reports.length >= 101 ){
+      return this.title = 'Bestest Manager';
+    }
+  }
+  
+  fire(index) {
+    super.fire(index);
+    this.bonus += 100;
+
+    if(this.reports.length === 0 ) {
+      return this.title = 'Not a manager';
+    } else if(this.reports.length >= 1 && this.reports.length <=3 ){
+      return this.title = 'Barely Manager';
+    }else if(this.reports.length >= 4 && this.reports.length <=10 ){
+      return this.title = 'Mostly Manager';
+    }else if(this.reports.length >= 11 && this.reports.length <=50 ){
+      return this.title = 'Manager';
+    }else if(this.reports.length >= 51 && this.reports.length <=100 ){
+      return this.title = 'Manager Plus';
+    }else if(this.reports.length >= 101 ){
+      return this.title = 'Bestest Manager';
+    }
+  }
+
+}
+
+let manager2 = new ProgressiveManager('Janaya','Hernandez');
+console.log(manager2);
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
